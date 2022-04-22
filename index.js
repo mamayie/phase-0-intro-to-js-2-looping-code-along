3f6480c3de0373b2ac7d178a667a6714b8953f3a
Mohammed Kwasi Appiah <mappiah319@gmail.com>
2022-04-22 21:47:54 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr,eventName){
+    const newArr = []
+    for(let i=0;i<arr.length;i++){
+  
+      newArr[i] = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`;
+  
+    }
+    return newArr;
+  }
+  
+  
+  function countDown(num){
+    let i=num;
+    while( i >= 0){
+      console.log(i);
+      i--
+    }
+  }
